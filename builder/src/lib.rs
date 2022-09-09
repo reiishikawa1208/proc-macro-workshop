5314d16e26c7c6213062552637d446f7a57ca07f
reiishikawa1208 <medama.0101@gmail.com>
2022-09-09 19:25:09 +0900

    done-3

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,63 +1,67 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, Data, DeriveInput, Fields, FieldsNamed};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
+    let struct_name = input.ident;
+    let builder_name = format_ident!("{}Builder", struct_name);
+    let fields = extract_struct_fields(&input.data);
+
+    let wrapped_fields_stream_iter = fields.named.iter().map(|field| {
+        let ty = &field.ty;
+        let ident = &field.ident;
+        quote! {
+            #ident: Option<#ty>
+        }
+    });
+    let initial_fileds_stream_iter = fields.named.iter().map(|field| {
+        let ident = &field.ident;
+        quote! {
+            #ident: None
+        }
+    });
+    let builder_fields_setter_stream_iter = fields.named.iter().map(|field| {
+        let ty = &field.ty;
+        let ident = &field.ident;
+        quote! {
+            fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                self.#ident = Some(#ident);
+                self
+            }
+        }
+    });
 
     let expanded = quote! {
-        pub struct CommandBuilder {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
+        pub struct #builder_name {
+            #(#wrapped_fields_stream_iter),*
         }
 
-        impl Command {
-            pub fn builder() -> CommandBuilder {
-                CommandBuilder {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
+        impl #struct_name {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    #(#initial_fileds_stream_iter),*
                 }
             }
         }
 
-        impl CommandBuilder {
-            fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
-                self
-            }
-
-            fn args(&mut self, args: Vec<String>) -> &mut Self {
-                self.args = Some(args);
-                self
-            }
-            fn env(&mut self, env: Vec<String>) -> &mut Self {
-                self.env = Some(env);
-                self
-            }
-            fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir = Some(current_dir);
-                self
-            }
-
-            pub fn build(&mut self) -> Result<Command, Box<dyn std::error::Error>> {
-                if self.executable.is_none() || self.args.is_none() || self.env.is_none() || self.current_dir.is_none() {
-                    Err("Error occured!".into())
-                } else {
-                    Ok(Command {
-                        executable: self.executable.clone().unwrap(),
-                        args: self.args.clone().unwrap(),
-                        env: self.env.clone().unwrap(),
-                        current_dir: self.current_dir.clone().unwrap(),
-                    })
-                }
-            }
-        };
+        impl #builder_name {
+            #(#builder_fields_setter_stream_iter)*
+        }
     };
 
-    TokenStream::from(expanded)
+    proc_macro::TokenStream::from(expanded)
+}
+
+fn extract_struct_fields(data: &Data) -> &FieldsNamed {
+    match *data {
+        Data::Struct(ref data) => match data.fields {
+            Fields::Named(ref fields) => fields,
+            _ => panic!("invalid fields"),
+        },
+        _ => panic!("invalid data"),
+        // Data::Enum(_) => {}
+        // Data::Union(_) => {}
+    }
 }
