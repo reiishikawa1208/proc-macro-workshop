433b44d8d2d471306f21a6ddf15958e01b8d7ea9
reiishikawa1208 <medama.0101@gmail.com>
2022-09-16 18:28:12 +0900

    done-6

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,9 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DeriveInput, Fields, FieldsNamed};
+use syn::{
+    parse_macro_input, Data, DeriveInput, Fields, FieldsNamed, GenericArgument, Path,
+    PathArguments, PathSegment, Type, TypePath,
+};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -12,8 +15,14 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let wrapped_fields_stream_iter = fields.named.iter().map(|field| {
         let ty = &field.ty;
         let ident = &field.ident;
-        quote! {
-            #ident: Option<#ty>
+        if is_option_type(&ty) {
+            quote! {
+                #ident: #ty
+            }
+        } else {
+            quote! {
+                #ident: Option<#ty>
+            }
         }
     });
     let initial_fileds_stream_iter = fields.named.iter().map(|field| {
@@ -25,17 +34,34 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_fields_setter_stream_iter = fields.named.iter().map(|field| {
         let ty = &field.ty;
         let ident = &field.ident;
-        quote! {
-            fn #ident(&mut self, #ident: #ty) -> &mut Self {
-                self.#ident = Some(#ident);
-                self
+        if is_option_type(&ty) {
+            let inner_type = option_inner_type(&ty);
+            quote! {
+                fn #ident(&mut self, #ident: #inner_type) -> &mut Self {
+                    self.#ident = Some(#ident);
+                    self
+                }
+            }
+        } else {
+            quote! {
+                fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                    self.#ident = Some(#ident);
+                    self
+                }
             }
         }
     });
     let builder_build_stream_iter = fields.named.iter().map(|field| {
+        let ty = &field.ty;
         let ident = &field.ident;
-        quote! {
-            #ident: self.#ident.clone().unwrap()
+        if is_option_type(&ty) {
+            quote! {
+                #ident: self.#ident.clone()
+            }
+        } else {
+            quote! {
+                #ident: self.#ident.clone().unwrap()
+            }
         }
     });
 
@@ -75,5 +101,37 @@ fn extract_struct_fields(data: &Data) -> &FieldsNamed {
         _ => panic!("invalid data"),
         // Data::Enum(_) => {}
         // Data::Union(_) => {}
+    }
+}
+
+fn is_option_type(ty: &Type) -> bool {
+    match last_path_segment(&ty) {
+        Some(path_seg) => path_seg.ident == "Option",
+        None => false,
+    }
+}
+
+fn option_inner_type(ty: &Type) -> &GenericArgument {
+    match last_path_segment(&ty) {
+        Some(PathSegment {
+            ident: _,
+            arguments: PathArguments::AngleBracketed(ref gen_arg),
+        }) => gen_arg.args.first(),
+        _ => None,
+    }
+    .expect("invalid option type")
+}
+
+fn last_path_segment(ty: &Type) -> Option<&PathSegment> {
+    match ty {
+        &Type::Path(TypePath {
+            qself: None,
+            path:
+                Path {
+                    segments: ref seg,
+                    leading_colon: _,
+                },
+        }) => seg.last(),
+        _ => None,
     }
 }
